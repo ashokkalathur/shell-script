124b7f505fcc16a58f682a1315506e2a33233c8b
ashokkalathur <ashokkalathur@gmail.com>
2024-02-27 05:13:31 +0530

    add 04 script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello $PERSON2,goodmoring"
+echo "$PERSON2: hi $PERSON1,very goodmorning"
+echo "$PERSON1: how r u $PERSON2"
+echo "$PERSON2: iam fine,how r u $PERSON1"
