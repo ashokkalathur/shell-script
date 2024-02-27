4b6ebc6b5a8e1cfd1fe7e3afa1ed328f4736f205
ashokkalathur <ashokkalathur@gmail.com>
2024-02-27 06:53:05 +0530

    add 05 script

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,5 +5,6 @@ echo "please enter your username"
 read -s USERNAME #the value entered above will be attached to USERNAME variable
 echo "username is: $USERNAME"
 
+echo "please enter your password"
 read -s PASSWORD #the value entered above will be attached to USERNAME variable
 echo "passwoord is: $PASSWORD"
