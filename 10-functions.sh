6c05d3e832bf2e6934ae33aff7262f18748f4fe8
ashokkalathur <ashokkalathur@gmail.com>
2024-02-27 17:29:33 +0530

    add 10 script

---
+++ 10-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDAtE(){
+    if [ $? -ne 0 ]
+then
+  echo "installing mysql is failed"
+  exit 1
+else
+  echo "installing mysql is success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then 
+  echo "ERROR:: please run this script with root access"
+  exit 1 #you can give other than 0
+else
+  echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
