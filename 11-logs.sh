90f76e99b7fbf315c55cc0583046c4e36f3a77d0
ashokkalathur <ashokkalathur@gmail.com>
2024-02-29 19:02:51 +0530

    change 11 script

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,6 +3,10 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%T)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script name: $0"
@@ -10,16 +14,16 @@ echo "script name: $0"
 VALIDATE(){
     if [ $1 -ne 0 ]
 then
-  echo "ERROR:: $2 ... Failed"
+  echo -e "ERROR:: $2 ...$R Failed $N"
   exit 1
 else
-  echo "$2 is success"
+  echo -e "$G $2 is $G success $N"
 fi
 }
 
 if [ $ID -ne 0 ]
 then 
-  echo "ERROR:: please run this script with root access"
+  echo -e " $R ERROR:: please run this script with root access $N"
   exit 1 #you can give other than 0
 else
   echo "you are root user"
